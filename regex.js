ce3558f23c123bd14f9ddd93e85e55fa5f966c03
Your Name <you@example.com>
2024-11-10 09:48:12 +0100

    step 2 done

--- regex.js
+++ regex.js
@@ -1,7 +1,13 @@
+// // ETAPE 1
+
+// const str = "J'utilise les expressions regulière pour retrouver des schémas de text au sein d'une chaine de caractères."
+// const exp = /\bde\b/g // b permet de spécifier les bordures du mot
+
+// console.log("compte le nombre de 'de' : ", str.match(exp).length)
+
+// // ETAPE 2
+
 const str = "J'utilise les expressions regulière pour retrouver des schémas de text au sein d'une chaine de caractères."
-const exp = /\bde\b/g // b permet de spécifier les bordures du mot
+const exp = /de/g // b permet de spécifier les bordures du mot
 
-console.log(str.match(exp).length)
-
-
-
+console.log("compte le nombre de 'de' ou 'des': ", str.match(exp).length)
