9750a45fd4e2c8696a5f0bb35b650eb6e14efd87
Your Name <you@example.com>
2024-11-10 09:54:23 +0100

    step 3 done

--- regex.js
+++ regex.js
@@ -5,9 +5,16 @@
 
 // console.log("compte le nombre de 'de' : ", str.match(exp).length)
 
-// // ETAPE 2
+// ETAPE 2
+
+// const str = "J'utilise les expressions regulière pour retrouver des schémas de text au sein d'une chaine de caractères."
+// const exp = /de/g // prend en compte le pattern de
+
+// console.log("compte le nombre de 'de' ou 'des': ", str.match(exp).length)
+
+// ETAPE 3
 
 const str = "J'utilise les expressions regulière pour retrouver des schémas de text au sein d'une chaine de caractères."
-const exp = /de/g // b permet de spécifier les bordures du mot
+const exp = /[ld]e/g // [] caractères optionnels en début de pattern 
 
-console.log("compte le nombre de 'de' ou 'des': ", str.match(exp).length)
+console.log("compte le nombre de 'de', 'des' ou 'les': ", str.match(exp).length)
